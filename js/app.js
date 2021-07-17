61be7e96f52a1a0974b415b8814b4986948c78db
Abdulhamid <76687038+Abdulhamid1993@users.noreply.github.com>
2021-07-17 14:06:02 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+let menuBtn = document.querySelector(".menu-btn")
+let body = document.querySelector("body")
+let haje = true
+
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle("open")
+    
+})
